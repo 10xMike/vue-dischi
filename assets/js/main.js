ed01dc0888e3583c757c690eb03918dcdb87a315
10xMike <michelegec@gmail.com>
2021-06-03 20:31:10 +0200

    Consegna esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,18 @@ Attraverso una chiamata ajax all'API di boolean https://flynn.boolean.careers/ex
 BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
 BONUS 2: Ordinare i dischi per anno di uscita.
 */
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    albums: [],
+  },
+  methods: {},
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        this.albums = response.data.response;
+      });
+  },
+});
